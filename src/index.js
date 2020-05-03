b295ef5cd63b85eed412d33249da5662c820c18b
Ikem Emmanuel <ikem43@gmail.com>
2020-05-03 18:06:58 +0100

    ### Reactbootstrap Installed ### React Router installed ### Topbar(aka Nav bar) Componet Added..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css'
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -10,9 +11,9 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
