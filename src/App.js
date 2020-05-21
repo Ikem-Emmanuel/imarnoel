2faf5193c409058ae0b6ff66ce3c6efa7504f9ce
Ikem Emmanuel <ikem43@gmail.com>
2020-05-21 04:18:27 +0100

    button title adjusted

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ export class App extends Component {
 										href={linkedinpage}
 										target='_blanl'
 										className='text-green-500 hover:text-green-100 hover:bg-green-400 border border-green-500 text-xs font-semibold rounded px-4 py-2 mr-6 leading-normal'>
-										View Projects
+										Linkedin Profile
 									</a>
 
 									<a
